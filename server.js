cc550317a3c339ad395f962471183bcab59e0bd0
unkind709 <superunkind@gmail.com>
2017-03-02 14:42:16 +0700

    Add files via upload
    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json');
+
+client.on('ready', () => {
+    console.log('I\'m Onlline');
+})
+
+client.on('message', message => {
+    if(message.content === 'ควย') {
+        message.channel.sendMessage('ควยพ่อง');
+    }
+});
+
+client.login(settings.token);
